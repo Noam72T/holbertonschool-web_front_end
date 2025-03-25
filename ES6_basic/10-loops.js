e807c64ff5d1fabe6c2a4b2840f1c5ae1ab8c97b
Noam72T <9823@holbertonstudents.com>
2025-03-25 09:48:52 +0100

    remove folder

--- ES6_basic/10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    for (const value of array) {
-      const index = array.indexOf(value);
-      array[index] = appendString + value;
-    }
-  
-    return array;
-  }
-  
